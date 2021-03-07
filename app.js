a84ac53ddcb521a80fd23e2274ef8f33d2f90ec5
Umesh <kuwar51@gmail.com>
2021-03-07 21:01:41 +0530

    location and local storage added

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 // event listner adding
 const themeswitcher = document.querySelector("#AutoTheme");
 const default_theme = localStorage.getItem('theme') || "theme-light";
+navigator.geolocation.getCurrentPosition((position) => {
+    let sunset = new Date().sunset(position.coords.latitude, position.coords.longitude);
+    let sunrise = new Date().sunrise(position.coords.latitude, position.coords.longitude);
+
+    if (isday(sunset, sunrise)) {
+setTheme('theme-light')
+    }
+    else{
+        setTheme('theme-dark') 
+    }
+    function isday(sunset, sunrise) {
+        const nowHours = new Date().getHours();
+        return nowHours >= sunrise.getHours() && nowHours < sunset.getHours();
+    }
+});
 setTheme(default_theme);
 themeswitcher.addEventListener('change', (event) => {
     setTheme(event.target.value);
@@ -8,7 +23,7 @@ themeswitcher.addEventListener('change', (event) => {
 });
 // for theme
 function setTheme(theme) {
-    theme=theme || 'theme-light';
+    theme = theme || 'theme-light';
     document.documentElement.className = theme;
     localStorage.setItem('theme', theme);
     themeswitcher.value = theme;
